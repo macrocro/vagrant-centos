f487f1ad3717399d4432b848595fa207918c1f42
macrocro <me@macrocro.com>
2013-11-03 01:29:33 +0900

    rubyの2.0-p247をインストールする設定をroles/web.rbに書いた

--- Berksfile
+++ Berksfile
@@ -1,4 +1,5 @@
 site :opscode
 
 cookbook "yum"
-
+cookbook "rbenv", git: "https://github.com/fnichol/chef-rbenv"
+cookbook "ruby_build", git: "https://github.com/fnichol/chef-ruby_build"
--- roles/web.rb
+++ roles/web.rb
@@ -7,5 +7,12 @@ run_list(
          "recipe[yum::remi]",
          "recipe[essential]",
          "recipe[users]",
-         "recipe[rbenv]"
+         "recipe[rbenv::system]",
+         "recipe[ruby_build]"
          )
+
+
+default_attributes(
+                   "rbenv" => {
+                     "rubies" => "2.0.0-p247"
+                   })
