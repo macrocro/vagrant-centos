c69d00f6f437e305a6fc9a44f3d394543a3d55b9
macrocro <me@macrocro.com>
2013-11-03 16:39:07 +0900

    create firewall init script

---
+++ site-cookbooks/ufw/recipes/init.rb
@@ -0,0 +1,13 @@
+bash "iptable init settings by ufw" do
+  only_if 'which ufw'
+  user "root"
+  code <<-EOH
+service iptables start
+iptables -F
+ufw reset
+ufw default deny
+ufw limit "#{node['ssh']['port']}"
+ufw allow 80/tcp
+yes | ufw enable
+EOH
+end
