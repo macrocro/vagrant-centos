af63a4bfa18b8eb1b2cea0c4b936f96bcf5e5d0b
macrocro <me@macrocro.com>
2013-11-04 03:44:56 +0900

    change settings for private_network and forwarded_port

--- roles/web.rb
+++ roles/web.rb
@@ -2,17 +2,24 @@ name 'web'
 description 'Web Server'
 
 run_list(
-         "recipe[yum]",
-         "recipe[yum::epel]",
-         "recipe[yum::remi]",
-         "recipe[essential]",
-         "recipe[users]",
-         "recipe[rbenv::system]",
-         "recipe[ruby_build]"
+         # "recipe[yum]",
+         # "recipe[yum::epel]",
+         # "recipe[yum::remi]",
+         # "recipe[essential]",
+         # "recipe[users]",
+         # "recipe[ruby_build]",
+         # "recipe[rbenv::system]"
+         "recipe[ufw::install]",
+         "recipe[ufw::init]",
+         "recipe[nginx]",
+         "recipe[ssh]"
          )
-
 
 default_attributes(
                    "rbenv" => {
                      "rubies" => "2.0.0-p247"
-                   })
+                   },
+                   "ssh" => {
+                     "port" => 22
+                   }
+                   )
