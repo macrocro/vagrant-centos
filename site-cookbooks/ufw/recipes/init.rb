e91739d8e8444a8c76b65a1cd8cce267cd11875c
macrocro <me@macrocro.com>
2013-11-08 10:14:45 +0900

    modified ufw

--- site-cookbooks/ufw/recipes/init.rb
+++ site-cookbooks/ufw/recipes/init.rb
@@ -2,12 +2,10 @@ bash "iptable init settings by ufw" do
   only_if 'which ufw'
   user "root"
   code <<-EOH
-service iptables start
-iptables -F
 ufw reset
 ufw default deny
 ufw allow "#{node['ssh']['port']}"
-ufw allow 80/tcp
+ufw allow 80
 yes | ufw enable
 EOH
 end
--- site-cookbooks/ufw/recipes/install.rb
+++ site-cookbooks/ufw/recipes/install.rb
@@ -21,3 +21,20 @@ python ./setup.py install
 chmod -R g-w /etc/ufw /lib/ufw /etc/default/ufw /usr/sbin/ufw
 EOH
 end
+
+template "/etc/init.d/ufw" do
+  user "root"
+  group "root"
+  source "ufw.erb"
+  mode 00755
+end
+
+bash "init iptable and ufw" do
+  user "root"
+  code <<-EOH
+service iptables stop
+service ufw start
+chkconfig iptables off
+chkconfig ufw on
+EOH
+end
