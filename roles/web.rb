a1b2c0f125e097d5871e8fbcd233f65f8522500a
macrocro <me@macrocro.com>
2013-11-08 11:59:17 +0900

    Create README and modify json files and Vagrantfile

--- roles/web.rb
+++ roles/web.rb
@@ -9,10 +9,20 @@ run_list(
          # "recipe[users]",
          # "recipe[ruby_build]",
          # "recipe[rbenv::system]"
+         "recipe[apache]",
+         "recipe[mysql]",
+         "recipe[php]",
+         "recipe[phpmyadmin]",
+         "recipe[ssh]",
          "recipe[ufw::install]",
-         "recipe[ufw::init]",
-         "recipe[nginx]",
-         "recipe[ssh]"
+         "recipe[ufw::init]"
+
+         #####################
+         # "recipe[nginx]",
+         #####################
+         # "recipe[webdicom]"
+         #####################
+
          )
 
 default_attributes(
