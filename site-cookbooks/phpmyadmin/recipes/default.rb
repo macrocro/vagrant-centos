13a98c4126f463f15f24bf7bf769a7233a0cf947
macrocro <me@macrocro.com>
2013-11-08 10:14:23 +0900

    add phpmyadmin

---
+++ site-cookbooks/phpmyadmin/recipes/default.rb
@@ -0,0 +1,20 @@
+#
+# Cookbook Name:: phpmyadmin
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package "phpmyadmin" do
+  action :install
+end
+
+template "/etc/httpd/conf.d/phpMyAdmin.conf" do
+  source "phpMyAdmin.conf.erb"
+  owner "root"
+  group "root"
+  mode 0644
+  notifies :restart, 'service[httpd]'
+end
