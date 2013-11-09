83e2f189464d24ae89668e079a5d5898ef43ccba
macrocro <me@macrocro.com>
2013-11-09 22:19:16 +0900

    his-gakuseiのclone用のdeployを書いた

--- site-cookbooks/ssh/recipes/default.rb
+++ site-cookbooks/ssh/recipes/default.rb
@@ -16,6 +16,39 @@ template "/etc/ssh/sshd_config" do
   source "sshd_config.erb"
   owner "root"
   group "root"
-  mode 0600
+  mode 0644
   notifies :reload, 'service[sshd]'
 end
+
+template "/etc/ssh/ssh_config" do
+  source "ssh_config.erb"
+  owner "root"
+  group "root"
+  mode 0644
+  notifies :restart, 'service[sshd]'
+end
+
+# data_ids = data_bag :users
+
+# data_ids.each do |id|
+#   u = data_bag_item :users, id
+
+#   u['username'] ||= u['id']
+
+#   # Set home to location in data bag,
+#   # or a reasonable default (/home/$user).
+#   if u['home']
+#     home_dir = u['home']
+#   else
+#     home_dir = "/home/#{u['username']}"
+#   end
+
+#   template "#{home_dir}/.ssh/config" do
+#     not_if "#{home_dir}/.ssh/config"
+#     source "config.erb"
+#     user u['username']
+#     group u['username']
+#     mode 00700
+#   end
+
+# end
